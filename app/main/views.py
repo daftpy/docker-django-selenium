865ce658758171fca4f4d6c35761b6fba063b00c
Alexander Flood <daftpy@gmail.com>
2022-02-13 22:42:39 -0600

    Used the annotate method on the index view querysets in order to fix index view urls.

--- app/main/views.py
+++ app/main/views.py
@@ -2,16 +2,25 @@ from audioop import reverse
 from itertools import chain
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
+from django.db.models import CharField, Value
 from upload.models import FileSubmission, LinkSubmission
 from .forms import RegistrationForm
 
-# Create your views here.
 def index(request):
+    # We get a list of file submissions and use annotate to add a
+    # submission_type field with the value 'file'. This is used to help
+    # build dynamic urls in the template.
     file_submission_list = FileSubmission.objects.filter(
-        private=False).order_by('-created_at')
-
+        private=False).order_by('-created_at').annotate(
+            submission_type=Value('file', output_field=CharField()
+        )
+    )
+    # We do the same as above for link submissions.
     link_submission_list = LinkSubmission.objects.filter(
-        private=False).order_by('-created_at')
+        private=False).order_by('-created_at').annotate(
+            submission_type=Value('link', output_field=CharField()
+        )
+    )
 
     # Combine the link submission and file submission querysets
     # order by created_at and reverse the list.
