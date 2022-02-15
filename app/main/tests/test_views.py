c67075ae5cb90753149f6c2773d3208e0ae36788
Alexander Flood <daftpy@gmail.com>
2022-02-15 02:24:22 -0600

    Added test to ensure index view annotates submission_type on queryset.

--- app/main/tests/test_views.py
+++ app/main/tests/test_views.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import resolve
 from main.views import index
-from upload.models import FileSubmission
+from upload.models import FileSubmission, LinkSubmission
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -73,3 +73,34 @@ class HomePageTest(TestCase):
         olderSubmission = response.content.decode().find(instance1.title)
         self.assertGreater(olderSubmission, latestSubmision)
         
+    def test_home_page_annotates_queryset_with_correct_submission_type(self):
+        video = SimpleUploadedFile(
+            "file.mp4", b"file_content", content_type="video/mp4"
+        )
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.client.login(
+            username='testuser',
+            password='testpassword'
+        )
+        instance1 = FileSubmission(
+            file=video,
+            title='Test Submission 1',
+            description="A test description",
+            author=self.user
+        )
+        instance2 = LinkSubmission(
+            title='Test Submission 2',
+            description='A test description',
+            author=self.user,
+            link='https://google.com'
+        )
+        instance1.save()
+        instance2.save()
+        response = self.client.get('/')
+        # Keep in mind the submission_list is sorted by -created_at
+        test_submissions = response.context['submission_list']
+        self.assertEqual(test_submissions[0].submission_type, 'link')
+        self.assertEqual(test_submissions[1].submission_type, 'file')
